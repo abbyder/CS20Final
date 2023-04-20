5bcde72bf46244b95d73550706bf684845268b69
erickim0223 <erickim0223@yahoo.com>
2023-04-20 19:18:46 -0400

    ingredients and db functional

--- profile.js
+++ profile.js
@@ -3,11 +3,11 @@ const express=require("express")
 const cheerio = require("cheerio")
 const fs = require("fs");
 const session = require("express-session");
-  
+
 // Creating express Router
 const router=express.Router()
   
-async function main(usn, data) {
+async function updateingred(usn, data) {
     var MongoClient = require('mongodb').MongoClient;
     var uri = "mongodb+srv://erickim:123@cluster0.wzucucu.mongodb.net/?retryWrites=true&w=majority";
     const client = new MongoClient(uri);
@@ -21,14 +21,36 @@ async function main(usn, data) {
         // check here if user already exists.
         let ingredients = [];
         for (i = 0; i < data['count']; i++) {
-            
+            ingredients.push(data["ingred" + i]);
         }
         // create a document to insert
         const result = await user.findOneAndUpdate(
             { "username" : usn },
-            { $set: {"ingredients" : ["filt41", "filt41"] }}
+            { $set: {"ingredients" : ingredients }}
         );
-    
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+async function getingred(usn) {
+    var MongoClient = require('mongodb').MongoClient;
+    var uri = "mongodb+srv://erickim:123@cluster0.wzucucu.mongodb.net/?retryWrites=true&w=majority";
+    const client = new MongoClient(uri);
+    //{useUnifiedTopology: true} ??? ^
+
+    try {
+        await client.connect();
+
+        const database = client.db("user_info");
+        const user = database.collection("users");
+        const cursor = user.find({ "username" : usn }).project({_id: 0, ingredients: 1});
+        array = await cursor.toArray();
+        if (array.length > 0) { 
+            return array[0].ingredients;
+        }
     } catch (e) {
         console.error(e);
     } finally {
@@ -48,19 +70,29 @@ router.get("/",(req,res,next)=>{
         var $ = cheerio.load(data);
 
         $(".usn").text(sessionuser + "'s Profile");
-        res.send($.html());
+        var ingredients = getingred(sessionuser).catch(console.error);
+        ingredients.then(x => { 
+            for (i = 0; i < x.length; i++) {
+                $("#prof-ingred-display").append('<div class="user-ingred"><button class="xBtn" onclick="deleteIngred(&quot;2&quot;)">X</button>' + x[i] + '</div>');
+                $("#ingred-hidden").append('<input type="hidden" name="ingred0" value="' + x[i] + '">');
+            }
+            res.send($.html());
+        });
     });
 })
 
 router.post('/', function(request, response, next){
-    console.log(request.body);
+    // console.log(request.body);
     const sessionuser = request.session.user;
     // //database stuff
-    main(sessionuser, request.body).catch(console.error);
-
+    //check which form being submitted
+    var ret = updateingred(sessionuser, request.body).catch(console.error);
+    ret.then(x => { 
+	    response.redirect("/profile");
+    });
     //reroute
 	// response.send(request.body);
-	response.redirect("/profile");
+	// response.redirect("/profile");
 });
 
 module.exports=router
