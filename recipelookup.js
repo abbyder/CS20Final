7da8ee03a80c2ca51b9ba81a75072ed5ddd5b9fd
erickim0223 <erickim0223@yahoo.com>
2023-04-25 23:36:16 -0400

    recipelookup membership

--- recipelookup.js
+++ recipelookup.js
@@ -75,6 +75,29 @@ async function getdiet(usn) {
     }
 }
 
+async function getmembership(usn) {
+    var MongoClient = require('mongodb').MongoClient;
+    var uri = "mongodb+srv://erickim:123@cluster0.wzucucu.mongodb.net/?retryWrites=true&w=majority";
+    const client = new MongoClient(uri);
+    //{useUnifiedTopology: true} ??? ^
+
+    try {
+        await client.connect();
+
+        const database = client.db("user_info");
+        const user = database.collection("users");
+        const cursor = user.find({ "username" : usn }).project({_id: 0, membership: 1});
+        array = await cursor.toArray();
+        if (array.length > 0) { 
+            return array[0].membership;
+        }
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
 // Handling login request
 router.get("/",(req,res,next)=>{
     const sessionuser = req.session.user;
@@ -88,39 +111,37 @@ router.get("/",(req,res,next)=>{
             if (err) throw err;
 
             var $ = cheerio.load(data);
-            
-            var ingredients = getingred(sessionuser).catch(console.error);
-            ingredients.then(x => { 
-                //NEED TO ADD SUBSCRIPTION THING HERE
-                for (i = 0; i < x.length; i++) {
-                    $("#listofingred").append('<input type="radio" name="ingredient" class="ingredient" value="' + x[i] + '" id="ingredient' + i + '"></input><label for="ingredient' + i + '">' + x[i] + '</label></br>');
-                }
-                var allergies = getallergies(sessionuser).catch(console.error);
-                allergies.then(y => {
-                    for (i = 0; i < y.length; i++) {
-                        $("#allergies").append('<li>' + y[i] + '</li>');
-                    }
-                    var diet = getdiet(sessionuser).catch(console.error);
-                    diet.then(z => {
-                        for (i = 0; i < z.length; i++) {
-                            $("#diet").append('<li>' + z[i] + '</li>');
+            var membership = getmembership(sessionuser).catch(console.error);
+            membership.then(w => {
+                var ingredients = getingred(sessionuser).catch(console.error);
+                ingredients.then(x => { 
+                    //Subscription
+                    if (w == "false") {
+                        for (i = 0; i < x.length; i++) {
+                            $("#listofingred").append('<input type="radio" name="ingredient" class="ingredient" value="' + x[i] + '" id="ingredient' + i + '"></input><label for="ingredient' + i + '">' + x[i] + '</label></br>');
                         }
-                        res.send($.html());
+                    } else {
+                        for (i = 0; i < x.length; i++) {
+                            $("#listofingred").append('<input type="checkbox" name="ingredient" class="ingredient" value="' + x[i] + '" id="ingredient' + i + '"></input><label for="ingredient' + i + '">' + x[i] + '</label></br>');
+                        }
+                    }
+                    var allergies = getallergies(sessionuser).catch(console.error);
+                    allergies.then(y => {
+                        for (i = 0; i < y.length; i++) {
+                            $("#allergies").append('<li>' + y[i] + '</li>');
+                        }
+                        var diet = getdiet(sessionuser).catch(console.error);
+                        diet.then(z => {
+                            for (i = 0; i < z.length; i++) {
+                                $("#diet").append('<li>' + z[i] + '</li>');
+                            }
+                            res.send($.html());
+                        })
                     })
-                })
-            });
+                });
+            })
         });
     }
 })
-
-// router.post('/', function(request, response, next){
-//     //database stuff
-//     main(request.body['username'], request.body['password']).catch(console.error);
-
-//     //reroute
-// 	// response.send(request.body);
-// 	response.redirect("/profile");
-
-// });
 
 module.exports=router
